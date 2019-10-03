0d8d01ad102a5c25a41e8dc9e6b3d41b43dc218c
tungntvn23 <46994732+tungntvn23@users.noreply.github.com>
2019-10-03 09:23:52 +0700

    Update openstack_centos6_resizefs.sh

--- openstack_centos6_resizefs.sh
+++ openstack_centos6_resizefs.sh
@@ -15,3 +15,4 @@ chmod +x 05-grow-root.sh
 
 dracut --force --include 05-grow-root.sh /mount --install 'echo awk grep fdisk sfdisk growpart partx e2fsck resize2fs' "$(ls /boot/initramfs-*)" $(ls /boot/|grep vmlinuz|sed s/vmlinuz-//g)
 rm -f 05-grow-root.sh
+reboot
